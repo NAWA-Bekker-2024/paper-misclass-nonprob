e3f77cb467b2e94aa53661c5f212d23c22ad1484
BERENZ <maciej.bereswewicz@ue.poznan.pl>
2024-08-23 11:24:27 +0100

    more on final simulation

--- sim-2-emp-motivated.R
+++ sim-2-emp-motivated.R
@@ -7,62 +7,72 @@ library(nonprobsvy)
 library(simputation)
 
 ## classification matrices from linear and transformer (test data)
-lin_vals <- c(
-  65.9, 21.6, 7.3, 1.3, 2.6, 0.0, 0.0, 0.4, 0.9,
-  3.1, 85.9, 5.3, 2.3, 1.9, 0.0, 1.3, 0.1, 0.2,
-  2.4, 20.5, 58.1, 4.6, 6.8, 0.0, 6.0, 1.2, 0.6,
-  1.1, 13.2, 5.7, 68.3, 3.8, 0.0, 1.5, 1.1, 5.3,
-  1.3, 5.8, 7.3, 1.8, 81.0, 0.0, 1.0, 0.0, 2.0,
-  0.0, 14.3, 0.0, 0.0, 0.0, 42.9, 42.9, 0.0, 0.0,
-  0.8, 2.3, 1.9, 1.0, 0.8, 0.0, 83.0, 4.1, 6.2,
-  0.0, 0.4, 2.3, 1.9, 0.4, 0.0, 19.0, 70.9, 5.0,
-  0.5, 0.5, 5.0, 4.1, 5.5, 0.0, 12.3, 2.3, 70.0
-)
+## true (rows) / predicted (columns)
+lim_mat <- matrix(c(
+  153, 50, 17, 3, 6, 0, 0, 1, 2,
+  40, 1094, 67, 29, 24, 0, 16, 1, 2,
+  12, 103, 292, 23, 34, 0, 30, 6, 3,
+  3, 35, 15, 181, 10, 0, 4, 3, 14,
+  5, 23, 29, 7, 323, 0, 4, 0, 8,
+  0, 1, 0, 0, 0, 3, 3, 0, 0,
+  4, 12, 10, 5, 4, 0, 430, 21, 32,
+  0, 1, 6, 5, 1, 0, 49, 183, 13,
+  1, 1, 11, 9, 12, 0, 27, 5, 154
+), nrow = 9, byrow = TRUE)
 
-lin_mat <- matrix(lin_vals, nrow = 9, ncol = 9, byrow = TRUE)
-lin_mat <- t(lin_mat[-6, -6]/rowSums(lin_mat[-6, -6]))
+lin_mat <- lim_mat[-6, -6]
 
-tra_vals <- values <- c(
-  75.4, 15.5, 4.3, 1.7, 2.2, 0.0, 0.4, 0.0, 0.4, 
-  2.7, 87.0, 5.5, 2.0, 2.0, 0.0, 0.6, 0.1, 0.0, 
-  2.2, 13.1, 68.6, 2.8, 5.0, 0.0, 5.6, 1.4, 1.4, 
-  1.1, 10.6, 6.0, 70.2, 2.3, 0.0, 0.4, 1.1, 8.3, 
-  1.0, 3.8, 6.3, 1.8, 85.7, 0.0, 0.3, 0.0, 1.3, 
-  14.3, 0.0, 0.0, 0.0, 0.0, 57.1, 0.0, 0.0, 28.6, 
-  0.6, 1.7, 1.9, 0.2, 0.2, 0.0, 88.2, 3.5, 3.7, 
-  0.0, 0.4, 1.2, 1.9, 0.4, 0.0, 17.4, 76.4, 2.3, 
-  0.5, 0.0, 2.7, 5.5, 5.9, 2.3, 8.6, 1.8, 72.7 
-)
+tra_mat <- matrix(c(
+  175, 36, 10, 4, 5, 0, 1, 0, 1,
+  35, 1108, 70, 25, 26, 0, 8, 1, 0,
+  11, 66, 345, 14, 25, 0, 28, 7, 7,
+  3, 28, 16, 186, 6, 0, 1, 3, 22,
+  4, 15, 25, 7, 342, 0, 1, 0, 5,
+  1, 0, 0, 0, 0, 4, 0, 0, 2,
+  3, 9, 10, 1, 1, 0, 457, 18, 19,
+  0, 1, 3, 5, 1, 0, 45, 197, 6,
+  1, 0, 6, 12, 13, 5, 19, 4, 160
+), nrow = 9, byrow = TRUE)
 
-tra_mat <- matrix(tra_vals, nrow = 9, ncol = 9, byrow = TRUE)
-tra_mat <- t(tra_mat[-6, -6]/rowSums(tra_mat[-6, -6]))
+tra_mat <- tra_mat[-6, -6]
+
+## genreate matrices for measurement error (pred in rows, true in columns)
+lin_mat_errors <- prop.table(t(lin_mat), margin = 2)
+lin_mat_errors[lin_mat_errors < 0.01] <- 0.01
+lin_mat_errors <- lin_mat_errors/colSums(lin_mat_errors)
+
+lin_mat_correction <- prop.table(lin_mat, margin = 2)
+lin_mat_correction[lin_mat_correction < 0.01] <- 0.01
+lin_mat_correction <- lin_mat_correction/colSums(lin_mat_correction)
+
+
+tra_mat_errors <- prop.table(t(tra_mat), margin = 2)
+tra_mat_errors[tra_mat_errors < 0.01] <- 0.01
+tra_mat_errors <- tra_mat_errors/colSums(tra_mat_errors)
+
+tra_mat_correction <- prop.table(tra_mat, margin = 2)
+tra_mat_correction[tra_mat_correction < 0.01] <- 0.01
+tra_mat_correction <- tra_mat_correction/colSums(tra_mat_correction)
 
 
 ## population data from the JVS
-
 pop_data <- fread("data-raw/sim-empirical-pop-data.csv")
 pop_data[, size := factor(size, c("Small", "Medium", "Large"))]
+## substractig more for 1:3 just for the simulation
+#pop_data[occup_code %in% 1:3 & prob_y, prob_y:=ifelse(prob_y+0.05 > 1, 1, prob_y+0.05)]
 pop_data_expanded <- pop_data[rep(pop_data[, .I], vac_total), .(private, size, occup_code, prob_y, prob_cbop)]
 pop_data_expanded[, occup_code:=as.factor(occup_code)]
 
-colnames(tra_mat) <- colnames(lin_mat) <- levels(pop_data_expanded$occup_code)
+colnames(tra_mat_errors) <- colnames(tra_mat_correction) <- colnames(lin_mat_errors) <- 
+  colnames(lin_mat_correction) <- levels(pop_data_expanded$occup_code)
 
 ## add mis-classification
 set.seed(2024)
 pop_data_expanded[, y:=rbinom(.N, 1, prob_y)]
-occup_lin <- simex::misclass(pop_data_expanded[, .(occup_code)], list(occup_code = lin_mat), k = 1)
-occup_tra <- simex::misclass(pop_data_expanded[, .(occup_code)], list(occup_code = tra_mat), k = 1)
+occup_lin <- simex::misclass(pop_data_expanded[, .(occup_code)], list(occup_code = lin_mat_errors), k = 1)
+occup_tra <- simex::misclass(pop_data_expanded[, .(occup_code)], list(occup_code = tra_mat_errors), k = 1)
 pop_data_expanded[, occup_code_lin := occup_lin$occup_code]
 pop_data_expanded[, occup_code_tra := occup_tra$occup_code]
-
-## for correction
-lin_mat_for_corr <- prop.table(margin = 2, xtabs(~occup_code+occup_code_lin, pop_data_expanded))
-lin_mat_for_corr[lin_mat_for_corr < 0.01] <- 0.01
-for (i in 1:ncol(lin_mat_for_corr)) lin_mat_for_corr[, i] <- lin_mat_for_corr[,i]/sum(lin_mat_for_corr[,i])
-tra_mat_for_corr <- prop.table(margin = 2, xtabs(~occup_code+occup_code_tra, pop_data_expanded))
-tra_mat_for_corr[tra_mat_for_corr < 0.01] <- 0.01
-for (i in 1:ncol(tra_mat_for_corr)) tra_mat_for_corr[, i] <- tra_mat_for_corr[,i]/sum(tra_mat_for_corr[,i])
-
 
 y_true <- mean(pop_data_expanded$y)
 
@@ -75,9 +85,9 @@ vcd::assocstats(xtabs(~y+ occup_code_tra, data = pop_data_expanded))
 
 
 ## simulation
-R <- 50
+R <- 250
 B_simex <- 50
-results <- matrix(0, nrow = R, ncol = 18, 
+results <- matrix(0, nrow = R, ncol = 19, 
                   dimnames = list(NULL, 
                                   c("naive", 
                                     "ipw", "mi", "dr",
@@ -85,20 +95,20 @@ results <- matrix(0, nrow = R, ncol = 18,
                                     "ipw_tra", "mi_tra", "dr_tra",
                                     "ipw_lin_cor", "mi_lin_cor", "dr_lin_cor",
                                     "ipw_tra_cor", "mi_tra_cor", "dr_tra_cor",
-                                    "mi_li", "mi_q")))
+                                    "mi_li", "mi_lo", "mi_q")))
 
-results_ci <- matrix(0, nrow = R, ncol = 17, 
+results_ci <- matrix(0, nrow = R, ncol = 18, 
                   dimnames = list(NULL, 
                                   c("ipw", "mi", "dr",
                                     "ipw_lin", "mi_lin", "dr_lin",
                                     "ipw_tra", "mi_tra", "dr_tra",
                                     "ipw_lin_cor", "mi_lin_cor", "dr_lin_cor",
                                     "ipw_tra_cor", "mi_tra_cor", "dr_tra_cor",
-                                    "mi_li", "mi_q")))
+                                    "mi_li", "mi_lo", "mi_q")))
 
 for (r in 1:R) {
   set.seed(r)
-  if (r %% 50 == 0) print(r)
+  print(r)
   pop_data_expanded[, cbop:=rbinom(.N, 1, prob_cbop)] 
   cbop_data <- pop_data_expanded[cbop == 1]
   prob_sample <- pop_data_expanded[sample(1:.N, 1000)]
@@ -106,8 +116,8 @@ for (r in 1:R) {
   prob_sample[, ":="(occup_code_lin=NULL, occup_code_tra=NULL)]
   
   ## imputation based mis-classification matrix 
-  occup_lin <- simex::misclass(prob_sample[, .(occup_code)], list(occup_code = lin_mat), k = 1)
-  occup_tra <- simex::misclass(prob_sample[, .(occup_code)], list(occup_code = tra_mat), k = 1)
+  occup_lin <- simex::misclass(prob_sample[, .(occup_code)], list(occup_code = lin_mat_correction), k = 1)
+  occup_tra <- simex::misclass(prob_sample[, .(occup_code)], list(occup_code = lin_mat_correction), k = 1)
   prob_sample_lin <- copy(prob_sample)
   prob_sample_lin[, occup_code := occup_lin$occup_code]
   prob_sample_tra <- copy(prob_sample)
@@ -236,21 +246,29 @@ for (r in 1:R) {
   formula = y ~ private + size + occup_code, family= "binomial")
   
   res_mi_lin_simex_l <- simex::mcsimex(model = mi_lin_m1, 
-                                       mc.matrix = list(occup_code = lin_mat_for_corr), 
-                                       SIMEXvariable = c("occup_code"), fitting.method	= "linear", asymptotic = FALSE, 
-                                       lambda = c(0.01, 0.05, 0.1), 
+                                       mc.matrix = list(occup_code = lin_mat_correction), 
+                                       SIMEXvariable = c("occup_code"), 
+                                       fitting.method	= "linear", 
+                                       asymptotic = FALSE, 
+                                       #lambda = c(0.005, 0.01, 0.05, 0.1, 0.2, 0.5), 
                                        jackknife.estimation	= FALSE, B = B_simex)
 
+  res_mi_lin_simex_ll <- refit(object = res_mi_lin_simex_l, 
+                              fitting.method = "loglinear", 
+                              asymptotic = FALSE, 
+                              jackknife.estimation	= FALSE)
+  
   res_mi_lin_simex_q <- refit(object = res_mi_lin_simex_l, 
                               fitting.method = "quadratic", 
                               asymptotic = FALSE, 
                               jackknife.estimation	= FALSE)
   
   prob_sample_svy <- update(prob_sample_svy,
-                    y_li = predict(res_mi_lin_simex_l, prob_sample_svy$variables, type = "response"),
-                    y_qu = predict(res_mi_lin_simex_q, prob_sample_svy$variables, type = "response"))
+                            y_li = predict(res_mi_lin_simex_l, prob_sample_svy$variables, type = "response"),
+                            y_lo = predict(res_mi_lin_simex_ll, prob_sample_svy$variables, type = "response"),
+                            y_qu = predict(res_mi_lin_simex_q, prob_sample_svy$variables, type = "response"))
   
-  res <- svymean(~y_li + y_qu, design = prob_sample_svy)
+  res <- svymean(~y_li + y_lo + y_qu, design = prob_sample_svy)
   res_ci <- confint(res)
   
   results[r, 1] <- mean(cbop_data$y)
@@ -272,6 +290,7 @@ for (r in 1:R) {
   results[r, 16] <- res_dr_tra_cor$output$mean
   results[r, 17] <- res[1]
   results[r, 18] <- res[2]
+  results[r, 19] <- res[3]
   
   
   results_ci[r, 1] <- res_ipw_cal$confidence_interval[1] < y_true & res_ipw_cal$confidence_interval[2] > y_true
@@ -291,18 +310,19 @@ for (r in 1:R) {
   results_ci[r, 15] <- res_dr_tra_cor$confidence_interval[1] < y_true & res_dr_tra_cor$confidence_interval[2] > y_true
   results_ci[r, 16] <- res_ci[1,1] < y_true & res_ci[1,2] > y_true
   results_ci[r, 17] <- res_ci[2,1] < y_true & res_ci[2,2] > y_true
+  results_ci[r, 18] <- res_ci[3,1] < y_true & res_ci[3,2] > y_true
   
 }
 
 boxplot(results[1:(r-1),]- y_true)
 abline(h=0,col = "red")
 
-colMeans(results_ci)
+colMeans(results_ci[1:(r-1),])
 
 
 ## 
-bias <- colMeans(results) - y_true
-var <- apply(results, 2, var)
+bias <- colMeans(results[1:(r-1),]) - y_true
+var <- apply(results[1:(r-1),], 2, var)
 rmse <- sqrt(bias^2+var)
 data.frame(bias,var, rmse)
 round(bias/y_true*100,2)
